79f0166976d130042faa5ecfc7b2cf61c21ccafd
GauravPandey892 <42074265+GauravPandey892@users.noreply.github.com>
2019-11-21 21:51:17 +0530

    Adding initial files

---
+++ home_fn.js
@@ -0,0 +1,21 @@
+// alert("hi");
+// $(function(){
+// 	$(document).one('click', '.like-review', function(e) {
+// 		$(this).html('<i class="fa fa-heart" aria-hidden="true"></i> You liked this');
+// 		$(this).children('.fa-heart').addClass('animate-like');
+// 	});
+// });
+// function counter()
+// {
+//     var c=0;
+//     c+=1;
+    
+// }
+
+var logo = document.getElementById("img1");
+
+logo.addEventListener("dblclick",function()
+{
+	//alert("hey");
+	window.open("colorgame.html");
+});
